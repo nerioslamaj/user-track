1d424efb980816084130a3172302acc4e0fc265d
nerioslamaj <nerios.lamaj@gmail.com>
2019-04-23 21:47:52 +0100

    Creating the source folder

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+require('libsvm-js').then(SVM => {
+  const SVM = new SVM(); // ...
+});
+
+var bayes = require('bayes')
+var NB = bayes()
+
 module.exports = function tiny(string) {
   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
   return string.replace(/\s/g, "");
